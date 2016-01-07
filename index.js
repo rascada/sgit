b109b583337f22b974b508ef08e558ca336a43c7
Damian <carftjr@gmail.com>
2016-01-07 03:02:22 +0100

    init cli

---
+++ index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let inquirer = require('inquirer');
+let sGit = require('commander');
+
+let pkg = require('./package');
+
+sGit
+  .usage(' ')
+  .version(pkg.version)
+  .parse(process.argv);
+
+let questions = [{
+    name: 'host',
+    message: 'host',
+    default: 'github',
+  }, {
+    name: 'scope',
+    message: 'user / organization:',
+    validate: scope => !scope ? `Can't clone without scope` : true,
+  }, {
+    name: 'repo',
+    message: 'repo:',
+    validate: repo => !repo ? `Can't clone nothing` : true,
+  },
+];
+
+inquirer.prompt(questions, function(answer) {
+  console.log(`git clone ${answer.host}/${answer.scope}/${answer.repo} ...`);
+});
