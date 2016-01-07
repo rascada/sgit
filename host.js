4e6bed9a2acf7e5f9c208aadff43965fdd6da8fa
Damian <carftjr@gmail.com>
2016-01-07 03:05:48 +0100

    add two popular repos

--- host.js
+++ host.js
@@ -1,7 +1,11 @@
 'use strict';
 
 module.exports = function host(host) {
-  switch (host.host) {
+  switch (host.toLowerCase()) {
+    case 'bitbucket':
+      return 'https://bitbucket.org';
+    case 'gitlab':
+      return 'https://gitlab.com';
     default:
       return 'https://github.com';
   }
