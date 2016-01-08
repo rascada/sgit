839ee7624b6bff95b0b280d83d59bd59f64781ba
Damian <carftjr@gmail.com>
2016-01-08 21:52:23 +0100

    modularize commands

---
+++ commands/clone/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let questions = require('./questions');
+let inquirer = require('inquirer');
+let host = require('./host');
+
+module.exports = {
+  description: 'interactive clone repo',
+  method() {
+    inquirer.prompt(questions, function(answer) {
+      let link = `${host(answer.host)}/${answer.scope}/${answer.repo}`;
+
+      Git
+        .Clone(link, answer.repo)
+        .then(function(repo) {
+          console.log(`Cloned ${answer.repo} successfully!`);
+        })
+        .catch(err => console.error(err));
+    });
+  },
+};
