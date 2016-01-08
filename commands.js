47698777f1e9607e4fd42e8f10106e3b3b9b6231
Damian <carftjr@gmail.com>
2016-01-08 21:45:40 +0100

    auto help commands

--- commands.js
+++ commands.js
@@ -5,16 +5,19 @@ let inquirer = require('inquirer');
 let host = require('./host');
 
 module.exports = {
-  clone() {
-    inquirer.prompt(questions, function(answer) {
-      let link = `${host(answer.host)}/${answer.scope}/${answer.repo}`;
+  clone: {
+    description: 'interactive clone repo',
+    method() {
+      inquirer.prompt(questions, function(answer) {
+        let link = `${host(answer.host)}/${answer.scope}/${answer.repo}`;
 
-      Git
-        .Clone(link, answer.repo)
-        .then(function(repo) {
-          console.log(`Cloned ${answer.repo} successfully!`);
-        })
-        .catch(err => console.error(err));
-    });
+        Git
+          .Clone(link, answer.repo)
+          .then(function(repo) {
+            console.log(`Cloned ${answer.repo} successfully!`);
+          })
+          .catch(err => console.error(err));
+      });
+    },
   },
 };
--- index.js
+++ index.js
@@ -8,12 +8,15 @@ let pkg = require('./package');
 let Git = require('nodegit');
 let sGit = require('yargs')
     .usage('Usage: sgit <command>')
-    .command('clone', 'interactive clone repo')
     .demand(1, ' must provide command!')
     .version(pkg.version)
     .help('help')
     .alias('help', 'h')
     .alias('version', 'v');
 
+Object.keys(commands)
+  .forEach(name =>
+    sGit.command(name, commands[name].description));
+
 if (sGit.argv._[0] && commands[sGit.argv._[0]])
-  commands[sGit.argv._[0]]();
+  commands[sGit.argv._[0]].method();
